ae3a441d601f99607f10e94e1f91213ad037174c
Hieu Bui <hibui@deloitte.ca>
2023-11-16 23:06:50 +0700

    Config Cors Policy

--- MetaWeatherApplication/Program.cs
+++ MetaWeatherApplication/Program.cs
@@ -15,6 +15,16 @@ builder.Services.Configure<ApiSettings>(configuration.GetSection("ApiSettings"))
 // Add service
 builder.Services.ConfigureServices();
 
+//services cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .Build());
+});
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,6 +40,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
